9486b9b7ad874054bcbbd4891018864bb27c3839
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-05-25 15:59:18 -0300

    Zero version

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,11 +1,5 @@
 import { memo } from 'react';
 
-import { Container } from './styles';
-
 export const Loading: React.FC = memo(() => {
-  return (
-    <Container>
-      <span>Splash Screen</span>
-    </Container>
-  );
+  return <span>Splash Screen</span>;
 });
